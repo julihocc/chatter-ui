37dc458730577aaf53fae6654335a4f46fe78ce4
julihocc <jcc30986@gmail.com>
2024-10-09 17:25:41 -0600

    Refactor App.tsx to use dark theme and update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { createTheme, ThemeProvider, CssBaseline, Container,  } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1>Dark Theme</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+          sagittis nunc. Cras sit amet nulla nec nunc ultricies fermentum.
+          Vivamus nec turpis auctor, ultrices nunc nec, ultricies odio. Donec
+          nec sapien nec nunc ultricies fermentum. Vivamus nec turpis auctor,
+          ultrices nunc nec, ultricies odio. Donec nec sapien nec nunc
+          ultricies fermentum.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </Container>
+    </ThemeProvider>
   );
 }
 
