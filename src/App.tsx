930608652ba9f3e93cc2411db3e4f6b6cf91d9dd
julihocc <jcc30986@gmail.com>
2024-10-10 12:55:46 -0600

    Refactor App.tsx to use react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import {
   CssBaseline,
   Container,
 } from "@mui/material";
-import Auth from "./components/auth/Auth";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Routes";
 
 const darkTheme = createTheme({
   palette: {
@@ -17,7 +18,7 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Container>
-        <Auth />
+        <RouterProvider router={router} />
       </Container>
     </ThemeProvider>
   );
