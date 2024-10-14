f4848f938a3927d06d233ee6d4d3dbb5f720375b
julihocc <jcc30986@gmail.com>
2024-10-14 16:36:26 -0600

    Refactor ApolloClient to use HttpLink and onError for error handling

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,7 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { API_URL } from "./urls";
 
+const httpLink = new HttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  }
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
 export const client = new ApolloClient({
+  link: errorLink.concat(httpLink),
   cache: new InMemoryCache(),
   uri: `${API_URL}/graphql`,
 });
