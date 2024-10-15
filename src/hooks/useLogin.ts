952b50f1820ff20dbb9afcf3e93542ba38a493b5
julihocc <jcc30986@gmail.com>
2024-10-15 17:23:50 -0600

    Refactor useLogin hook to handle login requests and error handling

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { API_URL } from "../constants/urls";
+import client from "../constants/apollo-client";
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export const useLogin = () => {
+  const [error, setError] = useState<boolean>();
+  const login = async (request: LoginRequest) => {
+    const res = await fetch(`${API_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    await client.refetchQueries({ include: "active" });
+  };
+
+  return { login, error };
+};
+
+export default useLogin;
