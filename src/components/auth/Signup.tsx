e6e543108f8315a9302ea44b43680706048d515d
julihocc <jcc30986@gmail.com>
2024-10-16 12:19:03 -0600

    Refactor Signup component to include error handling in Auth component

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -2,21 +2,28 @@ import { Link } from "react-router-dom";
 import { Link as MuiLink } from "@mui/material";
 import Auth from "./Auth";
 import useCreateUser from "../../hooks/useCreateUser";
+import { useState } from "react";
 
 export function Signup() {
   const [createUser] = useCreateUser();
+  const [error, setError] = useState("");
   return (
     <Auth
       submitLabel="Sign up"
       onSubmit={async ({ email, password }) => {
-        await createUser({
-          variables: {
-            createUserInput: {
-              email,
-              password,
+        try {
+          await createUser({
+            variables: {
+              createUserInput: {
+                email,
+                password,
+              },
             },
-          },
-        });
+          });
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
       }}
     >
       <Link to="/login" style={{ alignSelf: "center" }}>
