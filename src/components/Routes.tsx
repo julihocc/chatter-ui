448a48f0bb1293ca90876451a48797187d1bb16d
julihocc <jcc30986@gmail.com>
2024-10-16 16:32:26 -0600

    Add Home component to Routes

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
+import { Home } from "./home/Home";
 
 export const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
+  { path: "/", element: <Home /> },
 ]);
 
 export default router;
