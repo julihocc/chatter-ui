5569dfff624e4fc83ed2ad3ef046331675244233
julihocc <jcc30986@gmail.com>
2024-10-17 17:37:09 -0600

    Refactor ApolloClient URI configuration

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -4,8 +4,8 @@ import { API_URL } from "./urls";
 const uri = `${API_URL}/graphql`;
 
 export const client = new ApolloClient({
-  uri,
   cache: new InMemoryCache(),
+  uri,
   credentials: "include",
 });
 
