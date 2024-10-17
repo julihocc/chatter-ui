7d8dbdc8e4fbd2a2e21e5dcdbc10171bdcd49bc0
julihocc <jcc30986@gmail.com>
2024-10-17 17:35:45 -0600

    Refactor Routes component to add Guard for protecting routes

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -2,11 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
 import { Home } from "./home/Home";
+import { Guard } from "../components/auth/Guard";
 
 export const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
-  { path: "/", element: <Home /> },
+  {
+    path: "/",
+    element: (
+      <Guard>
+        <Home />
+      </Guard>
+    ),
+  },
 ]);
 
 export default router;
